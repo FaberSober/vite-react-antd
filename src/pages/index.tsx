02ffc80590a0a0e1301c25f68ae94153f0563077
xupengfei <faberxu@gmail.com>
2022-12-02 17:03:59 +0800

    demo01-drag and drop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
-import { Divider } from 'antd';
 import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default function App() {
-
   return (
-    <div>
-      <div>
-        <Link to="/demo01">demo01</Link>
-        <Divider type='vertical' />
-        <Link to="/sortable">sortable</Link>
-      </div>
+    <div className="storyDiv">
+      <Link to="/demo01">demo01: drag and drop</Link>
+      <Link to="/sortable">sortable</Link>
     </div>
   )
 }
