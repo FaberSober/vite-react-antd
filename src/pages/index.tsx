5ee17f2021d79cb3cf109dc09cd0eba11bbde4ba
xupengfei <faberxu@gmail.com>
2022-12-02 17:04:39 +0800

    dragAndDrop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 export default function App() {
   return (
     <div className="storyDiv">
-      <Link to="/demo01">demo01: drag and drop</Link>
+      <Link to="/dragAndDrop">drag and drop</Link>
       <Link to="/sortable">sortable</Link>
     </div>
   )
